fa5418a46978ed5b14edad7826697cd5b2791ea8
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2026-03-05 00:54:17 -0300

    Switch from Debian to Arch Linux in Termux script
    Compatibilidad fácil con yt-dlp

--- Util/Scripts/TermuxI.sh
+++ Util/Scripts/TermuxI.sh
@@ -17,63 +17,54 @@ apt update -y && yes | apt upgrade && pkg install -y proot-distro git wget termu
 
 
 
-proot-distro list | grep -q debian || proot-distro install debian
+proot-distro install archlinux
 
 cat > ~/.bashrc <<'EOF'
 termux-wake-lock
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[1;33m'
+N='\033[0m'
 
-echo -e "${YELLOW}Presiona una Tecla para Evitar el Inicio Automatico...${NC}"
+echo -e "${Y}⌨️Presiona una tecla para cancelar${N}"
+echo -e "${Y}⌨️Press any key to cancel${N}"
 
-timeout=5
+t=5
+c=0
 
-while [ $timeout -gt 0 ]; do
-    echo -ne "${YELLOW}Iniciando en $timeout segundos...\r${NC}"
-
-    if read -t 1 -n 1 keypress; then
-        echo -e "\n${RED}Inicio Automatico Cancelado.${NC}"
-
-        echo -e "${GREEN}Puedes Utilizar Termux Normalmente.${NC}"
-        echo -e "El bot esta en un Contenedor, Utiliza el Comando ${YELLOW}proot-distro login debian${NC} para Ingresar."
-        echo -e "${GREEN}Comandos Utiles:${NC}"
-        echo -e "${YELLOW}ls${NC} Visor de Archivos. ${YELLOW}cd${NC} Navegador de Archivos. ${YELLOW}msedit${NC} Creador y Editor de Texto. ${YELLOW}mkdir${NC} Creador de Carpetas/Directorios ${YELLOW}rm${NC} Borrar Archivos. Cada uno de estos Comandos se puede usar junto con ${YELLOW}--help para ver sus Funciones"
-        echo -e "${GREEN}Ejemplo:${NC}"
-        echo -e "Ir a la Carpeta del bot: ${YELLOW}cd levanter${NC}"
-        echo -e "Editar la Configuracion de Levanter: ${YELLOW}msedit config.env${NC} con Ctrl+O Guardas, Enter y Ctrl+X Salir de msedit."
-        echo -e "${GREEN} ¿Necesitas Ayuda? Contacta con Nosotros en Telegram @LevanterLyfe ${NC}"
-
-        echo -e "${RED} English ${NC}"
-        echo -e "\n${RED}Autostart Canceled.${NC}"
-        echo -e "${GREEN}You can use Termux normally.${NC}"
-        echo -e "The bot is in a container, use the command ${YELLOW}proot-distro login debian${NC} to log in."
-        echo -e "${GREEN}Useful Commands:${NC}"
-        echo -e "${YELLOW}ls${NC} File Viewer. ${YELLOW}cd${NC} File Browser. ${YELLOW}msedit${NC} Text Creator and Editor. ${YELLOW}mkdir${NC} Folder/Directory Creator ${YELLOW}rm${NC} Delete Files. Each of these commands can be used in conjunction with ${YELLOW}--help to see their Functions"
-        echo -e "${GREEN}Example:${NC}"
-        echo -e "Go to Bot Folder: ${YELLOW}cd levanter${NC}"
-        echo -e "Editing the Levanter Configuration: ${YELLOW}msedit config.env${NC} with Ctrl+O Save, Enter and Ctrl+X Exit msedit."
-        echo -e "${GREEN} Need Help? Contact Us on GitHub ${NC}"
-
-    
+while [ $t -gt 0 ]; do
+    echo -ne "${Y}Iniciando en $t... / Starting in $t...\r${N}"
+    if read -rsn1 -t 1 k; then
+        c=1
+        break
     fi
-
-    timeout=$((timeout - 1))
+    t=$((t-1))
 done
 
-echo -e "\n${GREEN}Iniciando aMuleD...${NC}"
-proot-distro login debian -- bash -c 'cd repo 2>/dev/null || true && chmod +x START.sh && (sleep 20 && /data/data/com.termux/files/usr/bin/termux-open http://localhost:8000) && ./START.sh'
+if [ $c -eq 1 ]; then
+echo
+echo -e "${R}Inicio cancelado / Autostart canceled${N}"
+echo -e "${G}Puedes usar Termux normalmente / You can use Termux normally${N}"
+echo -e "Bot en contenedor / Bot in container: ${Y}proot-distro login archlinux${N}"
 
+echo -e "Ir a carpeta / Go folder: ${Y}cd aMuleD.bin{N}"
+echo -e "${G}Ayuda / Help: https://github.com/weskerty/aMuleD.bin/discussions/categories/q-a${N}"
+return
+fi
+
+echo
+echo -e "${G}Iniciando aMuleD / Starting aMuleD${N}"
+
+proot-distro login archlinux -- bash -c 'cd repo 2>/dev/null || true && chmod +x START.sh && (sleep 20 && /data/data/com.termux/files/usr/bin/termux-open http://localhost:2685) && ./START.sh'
 
 EOF
 
-proot-distro login debian -- bash -c '
+proot-distro login archlinux -- bash -c '
 set -e
 
-apt-get update
-apt-get install -y git wget ffmpeg nodejs nano
+pacman -Syu --noconfirm
+pacman -S git wget ffmpeg nodejs nano python3 --noconfirm
 
 echo "[D] Repo MuLy"
 rm -rf aMuleD.bin
@@ -93,6 +84,6 @@ chmod +x START.sh
 ./START.sh &
 sleep 20
 echo "[D] Iniciando" 
- /data/data/com.termux/files/usr/bin/termux-open "localhost:8000"
+ /data/data/com.termux/files/usr/bin/termux-open "localhost:2658"
 '
 
